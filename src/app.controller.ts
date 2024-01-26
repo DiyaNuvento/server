ce82e7cac0fe6c0707d57ef80b329c8a10172198
Diya Dhinakaran <diya.dhinakaran@nuvento.net>
2024-01-26 17:03:04 +0530

    promises

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,8 +5,18 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('hello')
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('promises/sequential')
+  executePromisesSequentially() {
+    return this.appService.executePromisesSequentially();
+  }
+
+  @Get('promises/concurrent')
+  executePromisesConcurrently() {
+    return this.appService.executePromisesConcurrently();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,77 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  // Custom function 1
+  async function1(): Promise<string> {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve('Function 1 executed');
+      }, 1000);
+    });
+  }
+
+  // Custom function 2
+  async function2(): Promise<string> {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve('Function 2 executed');
+      }, 2000);
+    });
+  }
+
+  // Custom function 3
+  async function3(): Promise<string> {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve('Function 3 executed');
+      }, 3000);
+    });
+  }
+
+  //if the functions do not depend on each other and can be run in parallel, using Promise.all can be
+  //more efficient because it allows for concurrent execution. However, if the functions need to be run
+  //in a specific order, then they should be awaited sequentially
+
+  // Function to handle promises
+  async executePromisesSequentially(): Promise<{
+    result1: string;
+    result2: string;
+    result3: string;
+  }> {
+    try {
+      const result1 = await this.function1();
+      console.log(result1);
+
+      const result2 = await this.function2();
+      console.log(result2);
+
+      const result3 = await this.function3();
+      console.log(result3);
+      return { result1, result2, result3 };
+    } catch (error) {
+      console.error('Something went wrong:', error);
+    }
+  }
+
+  // Function to handle promises
+  async executePromisesConcurrently(): Promise<{
+    result1: string;
+    result2: string;
+    result3: string;
+  }> {
+    try {
+      const [result1, result2, result3] = await Promise.all([
+        this.function1(),
+        this.function2(),
+        this.function3(),
+      ]);
+      console.log(result1);
+      console.log(result2);
+      console.log(result3);
+      return { result1, result2, result3 };
+    } catch (error) {
+      console.error('Something went wrong:', error);
+    }
+  }
 }
